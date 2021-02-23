2f699457173aad79f1edde9d5448d5e5721def15
Conan Browne <Ventrik101@Gmail.com>
2021-02-23 15:36:57 -0500

    Done

--- index.js
+++ index.js
@@ -1,17 +1,43 @@
 class SortedList {
-  constructor() {}
-
-  add(item) {}
-
-  get(pos) {}
-
-  max() {}
-
-  min() {}
-
-  sum() {}
-
-  avg() {}
+	constructor() {
+		this.items = []
+		this.length = this.items.length
+	}
+	add(item) {
+		this.items.push(item)
+		this.length = this.items.length
+		this.items.sort((a, b) => a - b)
+	}
+	get(pos) {
+		if (this.items[pos - 1]) return this.items[pos]
+		else throw new Error('OutOfBounds')
+	}
+	max() {
+		if (this.items[this.items.length - 1])
+			return this.items[this.items.length - 1]
+		else throw new Error('EmptySortedList')
+	}
+	min() {
+		if (this.items.length === 0) {
+			throw new Error('EmptySortedList')
+		} else {
+			return this.items[0]
+		}
+	}
+	sum() {
+		if (this.items.length === 0) {
+			return 0
+		} else {
+			return this.items.reduce((a, b) => a + b)
+		}
+	}
+	avg() {
+		if (this.items.length === 0) {
+			throw new Error('EmptySortedList')
+		} else {
+			return this.sum() / this.items.length
+		}
+	}
 }
 
-module.exports = SortedList;
+module.exports = SortedList
